35146b71cde8a49280e947985846f9d0414f2e1f
Ediz Tefenlilioglu <ediz.tefenlilioglu@dyckerhoff.com>
2025-12-31 11:57:30 +0000

    🔧build(deps): add prost-types and google proto definitions

--- src/proto.rs
+++ src/proto.rs
@@ -1,11 +1,16 @@
 // Protocol buffer module
 // Include all generated proto modules
 pub mod vcsec;
-pub mod vehicle;
 pub mod car_server;
-pub mod common;
 pub mod errors;
 pub mod keys;
 pub mod managed_charging;
 pub mod signatures;
 pub mod universal_message;
+
+// The following modules are part of the CarServer package and are generated into car_server.rs
+pub use car_server as vehicle;
+pub use car_server as common;
+
+// Include google protobuf types
+pub mod google;
